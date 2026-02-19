6464f442c067ca6bc63d9f6ed4a89e2efdd36c54
adudley11 <adudley11@student.cscc.edu>
2026-02-18 22:36:43 -0500

    Added all 6 planets, space station and space ship

--- SpaceJam.py
+++ SpaceJam.py
@@ -18,8 +18,21 @@ class MyApp(ShowBase):
         self.Universe = self.loader.loadModel('./Assets/Universe/Universe.x')
         self.Universe.reparentTo(self.render)
         self.Universe.setScale(15000)
-        tex = self.loader.loadTexture("./Assets/Universe/Universe.jpg")
-        self.Universe.setTexture(tex, 1)
+        space = self.loader.loadTexture("./Assets/Universe/Universe.jpg")
+        self.Universe.setTexture(space, 1)
+        
+        self.Station = self.loader.loadModel('./Assets/SpaceStation/spaceStation.x')
+        self.Station.reparentTo(self.render)
+        self.Station.setPos(0, 4300, 0)
+        self.Station.setScale(10)
+        tex1 = self.loader.loadTexture("./Assets/SpaceStation/SpaceStation1_Dif2.png")
+        self.Station.setTexture(tex1, 1)
+        
+        self.Ship = self.loader.loadModel('./Assets/Spaceships/Dumbledore.x')
+        self.Ship.reparentTo(self.render)
+        self.Ship.setPos(0, 4300, 0)
+        tex2 = self.loader.loadTexture("./Assets/Spaceships/spacejet_C.png")
+        self.Station.setTexture(tex2, 1)
         
         self.Planet1 = self.loader.loadModel('./Assets/Planets/protoPlanet.x')
         self.Planet1.reparentTo(self.render)
@@ -27,7 +40,42 @@ class MyApp(ShowBase):
         self.Planet1.setScale(350)
         surf1 = self.loader.loadTexture("./Assets/Planets/Planet-1.jpg")
         self.Planet1.setTexture(surf1, 1)
-                
+
+        self.Planet2 = self.loader.loadModel('./Assets/Planets/protoPlanet.x')
+        self.Planet2.reparentTo(self.render)
+        self.Planet2.setPos(1125, 4300, 27)
+        self.Planet2.setScale(72)
+        surf2 = self.loader.loadTexture("./Assets/Planets/Planet-2.jpg")
+        self.Planet2.setTexture(surf2, 1)
+
+        self.Planet3 = self.loader.loadModel('./Assets/Planets/protoPlanet.x')
+        self.Planet3.reparentTo(self.render)
+        self.Planet3.setPos(215, 4300, 397)
+        self.Planet3.setScale(135)
+        surf3 = self.loader.loadTexture("./Assets/Planets/Planet-3.jpg")
+        self.Planet3.setTexture(surf3, 1)
+
+        self.Planet4 = self.loader.loadModel('./Assets/Planets/protoPlanet.x')
+        self.Planet4.reparentTo(self.render)
+        self.Planet4.setPos(13, 4300, 180)
+        self.Planet4.setScale(239)
+        surf4 = self.loader.loadTexture("./Assets/Planets/Planet-4.jpg")
+        self.Planet4.setTexture(surf4, 1)
+
+        self.Planet5 = self.loader.loadModel('./Assets/Planets/protoPlanet.x')
+        self.Planet5.reparentTo(self.render)
+        self.Planet5.setPos(222, 4300, 90)
+        self.Planet5.setScale(47)
+        surf5 = self.loader.loadTexture("./Assets/Planets/Planet-5.jpg")
+        self.Planet5.setTexture(surf5, 1)
+
+        self.Planet6 = self.loader.loadModel('./Assets/Planets/protoPlanet.x')
+        self.Planet6.reparentTo(self.render)
+        self.Planet6.setPos(116, 4300, 293)
+        self.Planet6.setScale(240)
+        surf6 = self.loader.loadTexture("./Assets/Planets/Planet-6.jpg")
+        self.Planet6.setTexture(surf6, 1)
         
+
 app = MyApp()
 app.run()
